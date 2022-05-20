205c5a1f9d6a9709bf3ab416f8c3fff391726d05
KimSoYeon <s2023@e-mirim.hs.kr>
2022-05-20 10:03:41 +0900

    #1 XML 주석 클래스, 메서드에 /// 을 사용하여 주석처리

--- CSClass3205/Car.cs
+++ CSClass3205/Car.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 namespace CSClass3205
 {
+    /// <summary>
+    /// 자동차 주차장 입차, 출차 시간 등을 지정하는 클래스입니다.
+    /// </summary>
     class Car
     {
         int carNumber;
         DateTime inTime;
         DateTime outTime;
 
+        /// <summary>
+        /// 뭔가 입력받고 출력합니다
+        /// </summary>
+        /// <param name="foo">뭔가 매개변수</param>
+        /// <returns>뭔가 리턴값</returns>
+        public int some(int foo)
+        {
+            return foo * 10;
+        }
+
+        /// <summary>
+        /// 자동차 입차 시간을 지정합니다.
+        /// </summary>
         public void SetInTime()
         {
             this.inTime = DateTime.Now;
         }
 
+        /// <summary>
+        /// 자동차 출차 시간을 지정합니다.
+        /// </summary>
         public void SetOutTime()
         {
             this.outTime = DateTime.Now;
